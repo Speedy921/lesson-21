d0d965569b5e211764eaa999c206c0d120694524
Artem Viatkin <newyorkbox47@gmail.com>
2023-01-20 23:40:35 +0200

    Добавьте файлы проекта.

---
+++ lesson 21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using lesson_21;
+
+Console.WriteLine("*****Fun with Custom Exceptins***\n");
+Car myCar = new Car("Rusty", 90);
+try
+{
+    //отслеживать исключение
+    myCar.Accelerate(50);
+}
+catch (CarIsDeadException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.ErrorTimeStamp);
+    Console.WriteLine(e.causeofError);
+  
+}
+Console.ReadLine();
